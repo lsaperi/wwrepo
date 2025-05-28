6350221f75818b8731a0bb4367c5f08036190b0d
lsaperi <lsaperi>
2025-05-28 13:40:27 -0300

    fdfds; TaskID: seq1

---
+++ TABLEs/Creation-Scripts/example.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `example` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(100) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
